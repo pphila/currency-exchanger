9d3d5414e747dfb54e7ae9378c18bf8c5d475af6
pphila <efl2ice89@gmail.com>
2022-10-22 13:53:53 -0700

    add refactor to if statement error handling and removed commented code

--- src/index.js
+++ src/index.js
@@ -3,23 +3,6 @@ import 'bootstrap';
 //import 'bootstrap/dist/css/bootstrap.mini.css';
 import './css/styles.css';
 
-// class CurrencyExchange {
-//   static async fetchCurrency(currency) {
-//     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`;
-//     try {
-//       const response = await fetch(url);
-//       console.log(response);
-//       if(response.status !== 200 && response.status === 400) {
-//         throw new Error(response.statusText)
-//       } else {
-//         return response.json();
-//       }
-//     } catch(error) {
-//       return error.response;
-//     }
-//   }
-// }
-
 async function runConverter(currency) {
   const response = await CurrencyExchange.fetchCurrency(currency);
   getData(response);
@@ -28,7 +11,9 @@ async function runConverter(currency) {
 
 function getData(response) {
   if (response.result !== "success") {
-    document.getElementById('#error-message').innerHTML(`Something went wrong, your result returned an ${response["error-type"]}`);
+    const errorDiv = document.getElementById('#error-message');
+    let errorHTMLString = `Something went wrong, your result returned an ${response["error-type"]}`;
+    errorDiv.innerText = errorHTMLString
   } else {
     let exchangeValue = parseFloat(response[`conversion_rate`]);
     let inputVal = parseFloat(document.querySelector('#US-currency-amount').value);
