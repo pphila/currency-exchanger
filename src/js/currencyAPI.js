0348cb2428680664c75df45055f1552a322484d6
pphila <efl2ice89@gmail.com>
2022-10-21 21:07:07 -0700

    add working async fetch request to currency API

---
+++ src/js/currencyAPI.js
@@ -0,0 +1,10 @@
+
+const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+  
+const fetchExchanger = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+fetchExchanger(); 
